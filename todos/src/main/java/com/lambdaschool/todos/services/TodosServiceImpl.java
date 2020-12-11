a40851c2bac7968d80c832007aa6c369a579157e
david chang <bjchang@Davids-MacBook-Air.local>
2020-12-11 10:24:25 -0800

    MVP

---
+++ todos/src/main/java/com/lambdaschool/todos/services/TodosServiceImpl.java
@@ -0,0 +1,25 @@
+package com.lambdaschool.todos.services;
+
+import com.lambdaschool.todos.models.Todo;
+import com.lambdaschool.todos.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+
+@Transactional
+@Service(value = "todosService")
+public class TodosServiceImpl implements TodosService
+{
+    @Autowired
+    private TodoRepository todorepos;
+
+    @Override
+    public void markComplete(long todoid)
+    {
+        Todo todo = todorepos.findById(todoid)
+                .orElseThrow(() -> new EntityNotFoundException("Todo " + todoid + " not found"));
+        todo.setCompleted(true);
+    }
+}
